8bf7008c9370c671512b4663628a1496887db440
quantumRaven23 <aldape.andres2@gmail.com>
2020-11-17 00:15:09 -0600

    Fixed regex, and KeyError bug

--- app.py
+++ app.py
@@ -31,9 +31,9 @@ def webhook_challenge():
 
 @app.route('/webhook/twitter',methods=['POST'])
 def respond_with_facts():
-    req=json.loads(request.get_json())
     print(type(request.get_json()))
     sys.stdout.flush()
+    req=json.loads(request.get_json())
     msg_txt=''
     cat_regex=re.compile(r'[Ss][Ee][Nn][Dd] [Mm][Ee] [Cc][Aa][Tt] [Ff][Aa][Cc][Tt][Ss]')
 
