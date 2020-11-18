728e0d8d75682e419d4abb57014463c091f00e57
quantumRaven23 <aldape.andres2@gmail.com>
2020-11-18 15:54:33 -0600

    Merged register-webhook.py and register-user.py together

---
+++ register_activity.py
@@ -0,0 +1,37 @@
+from decouple import config
+from urllib.parse import quote_plus
+from requests_oauthlib import OAuth1Session
+import json
+
+
+CONSUMER_KEY=config('consumer_key')
+CONSUMER_SECRET=config('consumer_secret')
+ACCESS_TOKEN=config('access_token')
+ACCESS_TOKEN_SECRET=config('access_token_secret')
+WEBHOOK_ID=config('wehook_id')
+YOUR_DOMAIN=config('website_domain')
+
+
+twitter = OAuth1Session(CONSUMER_KEY,
+                        client_secret=CONSUMER_SECRET,
+                        resource_owner_key=ACCESS_TOKEN,
+                        resource_owner_secret=ACCESS_TOKEN_SECRET)
+
+
+webhook_endpoint = quote_plus(YOUR_DOMAIN)
+url = 'https://api.twitter.com/1.1/account_activity/all/CatFactBot/'
+
+
+def register_webhook(url,twitter,webhook_endpoint):
+    url+=f'webhooks.json?url={webhook_endpoint}'
+    response = twitter.post(url)
+    print(response)
+
+def subscribe_to_user_activity(url,twitter):
+    url+=f'subscriptions.json'
+    response = twitter.post(url)
+    print(response)
+
+if __name__ == '__main__':
+    register_webhook(url,twitter,webhook_endpoint)
+    
