bce33f410dbcffe6de673181c3d24062dc3b4d3a
quantumRaven23 <aldape.andres2@gmail.com>
2020-11-17 20:16:17 -0600

    Testing Authentication

--- app.py
+++ app.py
@@ -57,19 +57,19 @@ def index():
 def validateRequest(request):
     req_headers = request.headers
     if req_headers.has_key('x-twitter-webhooks-signature'):
-        twitter_signature = bytes(req_headers['x-twitter-webhooks-signature'],'utf-8') #"sha256=lkasjdlksakdljkdsadsa"
+        twitter_signature = req_headers['x-twitter-webhooks-signature'] #"sha256=lkasjdlksakdljkdsadsa"
         
         consumer_secret_bytes = bytes(CONSUMER_SECRET,'utf-8') 
         payload_body = bytes(request.get_data(as_text=True),'utf-8')
 
         sha_256_digest = hmac.new(consumer_secret_bytes, payload_body , digestmod=hashlib.sha256).digest()
 
-        consumer_payload_b64 = "sha256="+base64.b64encode(sha_256_digest).decode('utf-8')
-        twitter_signature_b64 = base64.b64encode(twitter_signature).decode('utf-8')
+        consumer_payload_hash = "sha256="+base64.b64encode(sha_256_digest).decode('utf-8')
+        # twitter_signature_b64 = base64.b64encode(twitter_signature).decode('utf-8')
 
-        comparison_result = (consumer_payload_b64==twitter_signature_b64)
+        comparison_result = (consumer_payload_b64==twitter_signature)
 
-        print("HEY THIS IS THE COMPARISON RESULT",comparison_result,consumer_payload_b64,twitter_signature_b64)
+        print("HEY THIS IS THE COMPARISON RESULT",comparison_result,consumer_payload_hash,twitter_signature)
         sys.stdout.flush()
 
         if comparison_result:
