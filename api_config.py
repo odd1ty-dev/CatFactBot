3bd201c2dc0fb16a967a521b7de1ffd756d294ff
quantumRaven23 <aldape.andres2@gmail.com>
2021-12-17 19:45:33 -0600

    changed env variables

--- api_config.py
+++ api_config.py
@@ -6,6 +6,10 @@ def create_api():
     consumer_secret = os.getenv('consumer_secret')
     access_token = os.getenv('access_token')
     access_token_secret = os.getenv('access_token_secret')
+    print('C_K',consumer_key)
+    print('C_S',consumer_secret)
+    print('A_T',access_token)
+    print('A_T_S',access_token_secret)
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
